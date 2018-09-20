4c6179a70c80f721710e4c6d4a7813654ad53e21
Amr <32444508+amrsoudy@users.noreply.github.com>
2018-09-20 18:07:58 -0400

    up

---
+++ cour3/cour3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cour3.MvcApplication" Language="C#" %>
---
+++ cour3/cour3/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cour3.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int year { get; set; }
+    }
+}
